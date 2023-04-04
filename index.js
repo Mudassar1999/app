0f11907822ca083ec74923b0fd76fffccbf5329d
Mudassar Ansari <mudassaransari324@gmail.com>
2023-04-04 07:49:25 +0500

    Nodejs

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const app = require('./app');
-var x = 10;
-var z = 20;
+const http = require ('http');
+const colors = require ('colors');
 
-console.log(app.gender());
+console.log('Colors'.green);
+
+http.createServer((req,res)=>{
+    res.write('Assalam u Alaikum!');
+    res.end();
+}).listen(4500);
